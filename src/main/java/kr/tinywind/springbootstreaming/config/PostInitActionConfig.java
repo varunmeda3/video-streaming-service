bf073d2478037356194f32b330aa908ef3334d87
varunmeda3 <medavarun3@gmail.com>
2024-10-25 03:00:14 +0530

    PostInitActionConfig.java

--- src/main/java/kr/tinywind/springbootstreaming/config/PostInitActionConfig.java
+++ src/main/java/kr/tinywind/springbootstreaming/config/PostInitActionConfig.java
@@ -1,15 +1,1 @@
-package kr.tinywind.springbootstreaming.config;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.stereotype.Component;
-
-@Component
-public class PostInitActionConfig implements CommandLineRunner {
-    private static Logger logger = LoggerFactory.getLogger(PostInitActionConfig.class);
-
-    @Override
-    public void run(String... strings) throws Exception {
-    }
-}
